ff5794db4bca252e5ea028b4a49b56b529838720
Jono Hall <jonohall@gmail.com>
2021-01-02 16:20:58 +1300

    Update app.js

--- app.js
+++ app.js
@@ -3,33 +3,9 @@ const request = require('request')
 const rcon = require("./rcon/app.js")
 const SourceQuery = require('sourcequery')
 const fs = require('fs');
-
 const configdir = './config';
+
 const maxServers = 10;
-
-// Create dir if not exist
-if (!fs.existsSync(configdir)){
-    fs.mkdirSync(configdir);
-}
-
-// Create config file if not exist
-fs.readdir(configdir, (err, files) => {
-    try {
-        if (files.length < 1 )
-        var writeConfig = '{"debug":false,"token":"","apiSite":4,"apiUrl":"https://full uri here","serverIp":"","serverPort":"28015","enableRcon":"0","rconhost":"","rconport":"","rconpass":"","prefix":"!","roles":["Administrator","admins"],"queueMessage":"currently waiting in queue.","updateInterval":"3","showMap":false}'
-        var jsonData = JSON.parse(writeConfig);
-        
-        fs.writeFile("config/server1.json", JSON.stringify(jsonData, null, 2), 'utf8', function (err) {
-            if (err) {
-                console.log("An error occured while writing JSON Object to File.");
-                return console.log(err);
-            }
-            console.log("Config file created");
-        });   
-    } catch (error) {
-        
-    }
-});
 
 fs.readdir(configdir, (err, files) => {
 
